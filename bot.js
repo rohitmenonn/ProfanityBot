111e83774d7e72535ccdd6f64d1fe02215043e13
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2022-01-27 16:24:09 +0530

    Update bot.js
    Added: Group support, Delete message

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
-import { Telegraf } from 'telegraf';
+import pkg from 'telegraf';
+const { Telegraf } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
@@ -6,17 +7,21 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // create /start command
 bot.start((ctx) => {
-  ctx.reply(`Hi ${ctx.message.chat.first_name} ! Welcome to Profanity Bot`);
+  ctx.reply(`Hi ${ctx.message.from.first_name}! Welcome to Profanity Bot`);
 });
 
 bot.on('sticker', (ctx) => ctx.reply("Don't send me stickers noob!"));
 
 bot.on('text', (ctx) => {
   // Explicit usage
+  if(ctx.message.text == "toxic"){
+    ctx.reply('Message deleted! You are toxic.');
+    // ctx.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, {can_send_messages: false}, ctx.message.date + 400); 
+    ctx.deleteMessage(ctx.message.message_id, ctx.message.chat.id);
+  } 
 
   console.log(ctx.message, ctx.state);
   // Using context shortcut
-  ctx.reply(`Testing!`);
 });
 
 bot.launch();
