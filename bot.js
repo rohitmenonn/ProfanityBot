f370974414a3d793b9c7c757a6d2338bc5b6fc08
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2022-01-29 21:49:26 +0530

    Update bot.js
    Added: Timeout/Restrict functionality

--- bot.js
+++ bot.js
@@ -12,14 +12,24 @@ bot.start((ctx) => {
 
 bot.on('sticker', (ctx) => ctx.reply("Don't send me stickers noob!"));
 
+/* Todo: 
+
+1. Welcome message when users join
+2. Proper timeout message
+3. Diff timeouts for hateful, abusive etc. (optional)
+4. DM to owner when users repeat toxic behaviour (optional)
+
+*/
+
 bot.on('text', (ctx) => {
   // Explicit usage
   if(ctx.message.text == "toxic"){
-    ctx.reply('Message deleted! You are toxic.');
-    // ctx.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, {can_send_messages: false}, ctx.message.date + 400); 
-    ctx.deleteMessage(ctx.message.message_id, ctx.message.chat.id);
+    ctx.reply('Message deleted!');
+    // Results in perma timeout as of now, needs fix.
+    var timeout = ctx.message.date + 120;
+    ctx.restrictChatMember(ctx.message.chat.id, {user_id: ctx.message.from.id}, {can_send_messages: false}, timeout).catch(e => console.log(e)); 
+    ctx.deleteMessage(ctx.message.message_id, ctx.message.chat.id).catch(e => console.log(e));
   } 
-
   console.log(ctx.message, ctx.state);
   // Using context shortcut
 });
